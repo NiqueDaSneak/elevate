9c9a47eb7eb6120dbfaa037c10a443033b526362
niquedasneak <niquedasneak4@gmail.com>
2016-04-10 18:26:59 -0400

    loads more done, need to create portfolio io strategy

--- index.js
+++ index.js
@@ -1,5 +1,45 @@
 $(document).ready(function(){
-	$('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
+
+	$('.logo').animate({'opacity': 1}, 1500);
+
+
+	// menu button toggling
+	$('#nav-icon2').click(function(){
 		$(this).toggleClass('open');
 	});
+
+	$.fn.isOnScreen = function(){
+
+		var win = $(window);
+
+		var viewport = {
+			top : win.scrollTop(),
+			left : win.scrollLeft()
+		};
+		viewport.right = viewport.left + win.width();
+		viewport.bottom = viewport.top + win.height();
+
+		var bounds = this.offset();
+		bounds.right = bounds.left + this.outerWidth();
+		bounds.bottom = bounds.top + this.outerHeight();
+
+		return (!(viewport.right < bounds.left || viewport.left > bounds.right || viewport.bottom < bounds.top || viewport.top > bounds.bottom));
+
+	};
+
+	$(window).scroll(function() {
+		$('.lazyload').each(function() {
+				// debugger
+			if ($(this).isOnScreen()) {
+				$(this).addClass('lazyloaded');
+			}
+
+			// if($(this).isOnScreen == false) {
+			// 	$(this).removeClass('lazyloaded');
+			// }
+			
+		});
+		
+	});
+
 });
